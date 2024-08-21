c2888b89e2002493a2475c78e1899c3d7bf7b5aa
StefanWirsingStretta <45820616+StefanWirsingStretta@users.noreply.github.com>
2024-08-21 11:48:21 +0200

    ....

--- testfolders.py
+++ testfolders.py
@@ -1,11 +1,10 @@
 import imaplib
 
 def list_folders(email_address, password, server_address):
-    # Connect to the server
+   
     mail = imaplib.IMAP4_SSL(server_address)
     mail.login(email_address, password)
 
-    # List all folders
     result, folders = mail.list()
 
     if result == 'OK':
@@ -15,10 +14,10 @@ def list_folders(email_address, password, server_address):
     else:
         print("Failed to list folders.")
 
-    # Logout from the server
+ 
     mail.logout()
 
-# Replace with your actual email credentials
+
 email_address = 'info@stretta-music.com'
 password = '...'
 server_address = 'w01f596d.kasserver.com'
